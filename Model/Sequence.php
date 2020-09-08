513e55c2ad55a685f9a94fc4f651ac873a72d91d
Quazz <nickverschragen@gmail.com>
2020-09-08 18:04:24 +0200

    Create Sequence.php

---
+++ Model/Sequence.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Faonni\SalesSequence\Model;
+
+use Magento\Framework\App\ResourceConnection as AppResource;
+use Magento\Framework\DB\Sequence\SequenceInterface;
+
+/**
+ * Class Sequence represents sequence in logic
+ *
+ * @api
+ * @since 100.0.2
+ */
+class Sequence extends \Magento\SalesSequence\Model\Sequence
+{
+
+    /**
+     * Retrieve current value
+     *
+     * @return string
+     */
+    public function getCurrentValue()
+    {
+        if (!isset($this->lastIncrementId)) {
+            return null;
+        }
+
+        return sprintf(
+            $this->pattern,
+            $this->meta->getActiveProfile()->getPrefix(),
+            $this->calculateCurrentValue(),
+            $this->meta->getActiveProfile()->getSuffix()
+        );
+    }
+
+    /**
+     * Retrieve next value
+     *
+     * @return string
+     */
+    public function getNextValue()
+    {
+        $this->connection->insert($this->meta->getSequenceTable(), []);
+        $this->lastIncrementId = $this->connection->lastInsertId($this->meta->getSequenceTable());
+        return $this->getCurrentValue();
+    }
+
+    /**
+     * Calculate current value depends on start value
+     *
+     * @return string
+     */
+    private function calculateCurrentValue()
+    {
+        return ($this->lastIncrementId - $this->meta->getActiveProfile()->getStartValue())
+        * $this->meta->getActiveProfile()->getStep() + $this->meta->getActiveProfile()->getStartValue();
+    }
+}
