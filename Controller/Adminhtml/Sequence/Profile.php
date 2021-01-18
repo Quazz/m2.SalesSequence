8024ce66fe25e91a037d4992991ebd6746df6683
Quazz <nickverschragen@gmail.com>
2021-01-18 14:56:28 +0100

    Add missing constructor arguments

--- Controller/Adminhtml/Sequence/Profile.php
+++ Controller/Adminhtml/Sequence/Profile.php
@@ -12,6 +12,8 @@ use Magento\Framework\App\ActionInterface;
 use Magento\SalesSequence\Model\ProfileFactory;
 use Magento\SalesSequence\Model\MetaFactory;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Controller\Result\RedirectFactory;
 
 /**
  * Sequence Profile Controller
@@ -24,61 +26,65 @@ abstract class Profile implements ActionInterface
      * @see _isAllowed()
      */
     const ADMIN_RESOURCE = 'Faonni_Sequence::profile';
-	
+
     /**
      * Active Menu Path
      */
     const ACTIVE_MENU = 'Faonni_Sequence::profile';
-	
+
     /**
      * Core registry
      *
      * @var \Magento\Framework\Registry
      */
     protected $_coreRegistry;
-	
+
     /**
      * Sequence Profile
      *
      * @var \Magento\SalesSequence\Model\Profile
      */
     protected $_profile;
-    
+
     /**
      * Sequence Meta
      *
      * @var \Magento\SalesSequence\Model\Meta
      */
     protected $_meta;
-    
+
     /**
      * Result Page Factory
      *
      * @var PageFactory
      */
     protected $resultPageFactory;
-    
+
     /**
      * Initialize Controller
-	 *
+     *
      * @param Context $context
      * @param Registry $coreRegistry
      * @param ProfileFactory $profileFactory
-     * @param MetaFactory $metaFactory 
-     * @param PageFactory $resultPageFactory     
+     * @param MetaFactory $metaFactory
+     * @param PageFactory $resultPageFactory
      */
     public function __construct(
         Registry $coreRegistry,
         ProfileFactory $profileFactory,
         MetaFactory $metaFactory,
         PageFactory $resultPageFactory,
-        RequestInterface $request
+        RequestInterface $request,
+        ManagerInterface $messageManager,
+        RedirectFactory $resultRedirectFactory
     ) {
         $this->_coreRegistry = $coreRegistry;
         $this->_profile = $profileFactory->create();
         $this->_meta = $metaFactory->create();
         $this->resultPageFactory = $resultPageFactory;
         $this->request = $request;
+        $this->messageManager = $messageManager;
+        $this->resultRedirectFactory = $resultRedirectFactory;
     }
 
     /**
